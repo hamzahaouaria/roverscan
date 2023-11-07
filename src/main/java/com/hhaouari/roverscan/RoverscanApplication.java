c4c6d0b3712db2179a2ceb11dbad7d143f3d0951
Hamza Haouaria <hamzahaouaria@gmail.com>
2023-11-07 14:40:28 +0100

    Initialize project From Spring Initializr

---
+++ src/main/java/com/hhaouari/roverscan/RoverscanApplication.java
@@ -0,0 +1,13 @@
+package com.hhaouari.roverscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoverscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoverscanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhaouari/roverscan/RoverscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhaouari.roverscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoverscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
