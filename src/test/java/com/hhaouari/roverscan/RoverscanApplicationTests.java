749ac6cf6e96209429988857bcd060177d8b6c43
Hamza Haouaria <hamzahaouaria@gmail.com>
2023-11-07 14:46:47 +0100

    Assert that tests are working in the project   - Envirement is ready to go

--- src/test/java/com/hhaouari/roverscan/RoverscanApplicationTests.java
+++ src/test/java/com/hhaouari/roverscan/RoverscanApplicationTests.java
@@ -2,12 +2,19 @@ package com.hhaouari.roverscan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class RoverscanApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+	}
+
+	@Test
+	void testWorkingTests() {
+		Assert.isTrue(true, "Tests Are Working ok.");
 	}
 
 }
